8e4be436fd3e9bad200ed4ee2563ec018acea0a3
Alexander Liteplo <alexanderliteplo@gmail.com>
2022-10-16 13:41:34 -0700

    Made graph for part a

--- graphing.py
+++ graphing.py
@@ -1,27 +1,48 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# x axis values
-x = [1, 2, 3, 4, 5, 6]
-# corresponding y axis values
-y = [2, 4, 1, 5, 2, 6]
+
 
 data = pd.read_csv("./rs_1.csv")
 
-print(data)
+array = data.to_numpy()
+y = []
+rTrue = 0
+rFalse = 0
 
+
+
+def yFunction(rTrue, rFalse):
+    total = rTrue + rFalse
+    return rTrue / total
+
+
+for value in array:
+    if value[0] == 1:
+        rTrue += 1
+    if value[0] == 2:
+        rFalse += 1
+    y.append(yFunction(rTrue, rFalse))
+
+x = []
+for i in range(1, 100000):
+    x.append(i)
+    # print(i)
+
+
+print(rTrue + rFalse)
 # plotting the points
-plt.plot(x, y, color='green', linestyle='dashed', linewidth = 3,
-          marker='o', markerfacecolor='blue', markersize=12)
+plt.plot(x, y, color='blue', linestyle='solid', linewidth = 1,
+          marker=',', markerfacecolor='blue', markersize=2)
 
 # setting x and y axis range
-plt.ylim(0,1)
+plt.ylim(0,0.5)
 plt.xlim(1,100000)
-
+plt.xscale('log')
 # naming the x axis
-plt.xlabel('x - axis')
+plt.xlabel('Number of samples')
 # naming the y axis
-plt.ylabel('y - axis')
+plt.ylabel('P(R = T | s+, w+)')
 
 # giving a title to my graph
 plt.title('Q2. (a)')
